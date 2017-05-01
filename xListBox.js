95594ec0e684adee42c3bd535b49da7a872505c7
knightsforce <andrianov44@gmail.com>
2017-05-01 16:44:58 +0200

    Перевел на хранение с элементов на сам список

--- xListBox.js
+++ xListBox.js
@@ -1,5 +1,10 @@
 (function($) {
-
+/*
+var allItems = parentList.find(".xlistbox-item");
+var itemsArray = parentOptions.items;
+	var index = allItems.index(currentItem);
+	var currEl = itemsArray[index];
+*/
 var methods = {
 	option: function(key, val) {
 		var result = null;
@@ -7,19 +12,19 @@ var methods = {
 			case "string":
 			
 				if(val !== undefined) {
-					$.data(this.get(0), key, val);
+					this.data(key, val);
 					result = rerenderList(this);
 				} else {
-					result = $.data(this.get(0), key);
+					result = this.data(key);
 				}
 				break;
 			case "object":
-				var data = $.data(this.get(0));
+				var data = this.data();
 				var obj = key;
 				for(var i in obj) {
 					data[i] = obj[i];
 				}
-				$.data(this.get(0), data);
+				this.data(data);
 				result = rerenderList(this);
 				
 				break;
@@ -34,30 +39,27 @@ var methods = {
     	if(getType(items) == "array") {
     		result = rerenderList(this, items);
     	} else {
-    		var itemsArray = [];
-    		this.find(".xlistbox-item").each(function(i, item) {
-    			itemsArray.push($.data(item));
-    		});
-    		result = itemsArray;
+    		result = this.data().items;
     	}
     	return result;
     },
     getSelected: function() {
+
+    	var itemsArr = this.data().items;
     	return this.find(".xlistbox-item").filter(function(i, item){
-				return !!$.data(item, "selected");
+				return !!itemsArr[i]["selected"];
 		});
     },
     getItem: function(value) {
-    	var xlistboxItems = this.find(".xlistbox-item");
+    	var itemsArr = this.data().items;
     	var result = this;
     	if(typeof value == "object") {
-    		value = $.data(value.get(0), "value");
+    		value = value.data("value") || JSON.parse(value.attr("data-options"))["value"];
     	}
     	var options = null;
-		for(var i = 0; i<xlistboxItems.length; i++) {
-			options = $.data(xlistboxItems.get(i));
-			if(options.value == value) {
-				result = options;
+		for(var i = 0; i<itemsArr.length; i++) {
+			if(itemsArr[i]["value"] == value) {
+				result = itemsArr[i];
 				break;
 			}
 		}
@@ -69,16 +71,20 @@ var methods = {
     	}
 
     	var options = null;
-    	var xlistboxItems = this.find(".xlistbox-item");
+    	var data = this.data();
+    	var itemsArr = data.items;
 
-		xlistboxItems.each(function(i, item) {
-			options = $.data(item);
-			if(options.value != value) return;
-			for(let i in opt) {
-				options[i] = opt[i];
+    	for(var i = 0; i<itemsArr.length; i++) {
+    		options = itemsArr[i];
+    		if(options.value != value) continue;
+
+    		for(let key in opt) {
+				options[key] = opt[key];
 			}
-			$.data(item, options);
-		});
+    	}
+    	data.items=itemsArr;//По ссылке, так что не обязательно, это на случай непредвиденног
+    	this.data(data);
+
 		return rerenderList(this);
     }
 };
@@ -124,7 +130,7 @@ function parseDataFromDOM(el) {//dataOption
 	var options = null;
 
 	listItem.each(function(i, item) {
-		options = $.extend({}, $.data(item));
+		options = $.extend({}, $(item).data());
 		item = $(item);
 		options["dataOptions"] = item.attr("data-options");
 		options.label = item.find(".xlistbox-labeltext").html();//Можно .text(), но тогда теги не будут учитываться
@@ -138,7 +144,9 @@ function parseDataFromDOM(el) {//dataOption
 };
 
 function createList(data, elem) {
-	var xlistboxOptions = JSON.stringify(data);
+	var options = data.options || data;
+	var xlistboxOptions = JSON.stringify(options);
+	delete data.options;
 	var xlistbox = null;
 	
 	if(!elem) {
@@ -151,9 +159,9 @@ function createList(data, elem) {
 	xlistbox.attr("data-options", xlistboxOptions);
 	xlistbox.append(createElems(data));
 
-	if(data["disabled"]) xlistbox.addClass("xlistbox-disable");
-	else xlistbox.removeClass("xlistbox-disable");
-	$.data(xlistbox.get(0), data);
+	if(data["disabled"]) xlistbox.addClass("xlistbox-item-disable");
+	else xlistbox.removeClass("xlistbox-item-disable");
+	xlistbox.data(data);
 	return xlistbox;
 }
 
@@ -162,32 +170,32 @@ function rerenderList(elem, items) {
 	/*
 		На пересоздание с нуля уйдет меньше секунды при элементах = 5000
 	*/
-	var options = $.extend({}, $.data(elem.get(0)));
-	console.log("Выше")
+	var data = $.extend({}, $(elem).data());
+
 	if(items) {
-		options.items = items;
-	} else {
-		options.items = parseDataFromDOM(elem, true);
+		data.items = items;
+	} else if(!data.items.length){
+		data.items = parseDataFromDOM(elem);
 	}
 
-	return createList(options, elem);
+	return createList(data, elem);
 }
 
 function createElems(data) {
 
 	var li = $("<li class='xlistbox-item'/>");
 
-	var label = $("<div class='xlistbox-label'/>");
+	var label = $("<div class='xlistbox-item-label'/>");
 
-	var labelText = $("<div class='xlistbox-labeltext'/>");
+	var labelText = $("<div class='xlistbox-item-labeltext'/>");
 
-	var checkbox = $("<div class='xlistbox-checkbox'>"+
-			"<div class='xlistbox-flag'></div>"+
+	var checkbox = $("<div class='xlistbox-item-checkbox'>"+
+			"<div class='xlistbox-item-flag'></div>"+
 			"</div>");
 
-	var itemDirection = $("<div class='xlistbox-direction'>"+
-			"<div class='xlistbox-forward'>&#8593;</div>"+
-			"<div class='xlistbox-backward'>&#8595;</div>"+
+	var itemDirection = $("<div class='xlistbox-item-direction'>"+
+			"<div class='xlistbox-item-forward'>&#8593;</div>"+
+			"<div class='xlistbox-item-backward'>&#8595;</div>"+
 			"</div>");
 
 	var arrList=[];
@@ -213,7 +221,7 @@ function createElems(data) {
 		text = options.label || "";
 		//delete options.label;
 
-		options = (options["dataOption"]) ? options["dataOption"] : JSON.stringify(options);
+		options = (options["dataOption"]) ? JSON.stringify(options["dataOption"]) : JSON.stringify(options);
 		
 		_label = label.clone();
 
@@ -223,23 +231,23 @@ function createElems(data) {
 		_li = li.clone()
 		.attr("data-options", options)
 		.append(_label);
-		
+
 		if(parentMovable && movable) {
 			_li.append(itemDirection.clone());
 		}
 		
 		if(disabled) {
-			_li.addClass("xlistbox-disabled");
+			_li.addClass("xlistbox-item-disabled");
 		}
 		if(multiselect && selected) {
 			
-			_li.addClass("xlistbox-selected");
+			_li.addClass("xlistbox-item-selected");
 
 		} else if(oneSelect && selected) {
 			oneSelect = false;
-			_li.addClass("xlistbox-selected");
+			_li.addClass("xlistbox-item-selected");
 		}
-		$.data(_li.get(0), items[i]);
+
 		arrList.push(_li);
 	}
 	return arrList;
@@ -252,52 +260,68 @@ function getType(obj) {
 
 function handlerClick(e) {
 	var target = $(e.target);
-	var currentItem = $(target).closest('.xlistbox-item');
-	var options = $.data(currentItem.get(0)) || {};
-	var parentOptions = $.data(currentItem.closest(".xlistbox").get(0)) || {};
-	if(parentOptions["disabled"] || options["disabled"]) return;
 
+	var currentItem = $(target).closest(".xlistbox-item");
+	var parentList = currentItem.closest(".xlistbox");
+
+	var parentOptions = parentList.data() || {};
+
+	var allItems = parentList.find(".xlistbox-item");
+	var itemsArray = parentOptions.items;
+	var index = allItems.index(currentItem);
+	var currEl = itemsArray[index];
+	
+	if(parentOptions["disabled"] || currEl.disabled) return;
+
+	
 	var multiselect = parentOptions["multiselect"];
 	var prevItem, nextItem, itemArr;
-
 		/*
 			closest вместо hasClass, если target вложенный узел
 		*/
 
-	if(target.closest(".xlistbox-forward").length) {
-			
-		prevItem = currentItem.prev();
+	if(target.closest(".xlistbox-item-forward").length) {
 
-		if(!prevItem.length || !$.data(prevItem.get(0))["movable"]) return;
+		prevItem = currentItem.prev();
+		var prevEl = itemsArray[index-1];
+
+		if(!prevItem.length || !prevEl["movable"]) return;
 		prevItem.before(currentItem);
 		currentItem.closest(".xlistbox").trigger("change");
 
-	} else if(target.closest(".xlistbox-backward").length) {
+		itemsArray[index-1] = currEl;
+		itemsArray[index] = prevEl;
+
+	} else if(target.closest(".xlistbox-item-backward").length) {
 
 		nextItem = currentItem.next();
-		if(!nextItem.length || !$.data(nextItem.get(0))["movable"]) return;
+		var nextEl = itemsArray[index+1];
+
+		if(!nextItem.length || !nextEl["movable"]) return;
 		nextItem.after(currentItem);
 		currentItem.closest(".xlistbox").trigger("change");
 
-	} else if(target.closest(".xlistbox-label").length) {
+		itemsArray[index] = nextEl;
+		itemsArray[index+1] = currEl;
+
+	} else if(target.closest(".xlistbox-item-label").length) {
 
 		if(!parentOptions["selectable"]) return;
 			/*
 				Можно объединить в один if, но это лишние манипуляции с DOM
 			*/
 
-		currentCheckbox = currentItem.find(".xlistbox-checkbox");
-		itemArr = currentItem.closest(".xlistbox").find(".xlistbox-item");
-		if(!multiselect && itemArr.filter(".xlistbox-selected").length && !currentItem.hasClass("xlistbox-selected")) {
+		currentCheckbox = currentItem.find(".xlistbox-item-checkbox");
+		if(!multiselect && allItems.filter(".xlistbox-item-selected").length && !currentItem.hasClass("xlistbox-item-selected")) {
 			return;
 		}
 
-		currentItem.toggleClass("xlistbox-selected");
+		currentItem.toggleClass("xlistbox-item-selected");
 
-		if(options["selected"]) {
-			delete options["selected"];
+		if(currEl.selected) {
+			itemsArray[index]["selected"]=false;
 		} else {
-			options["selected"]=true;
+			itemsArray[index]["selected"]=true;
 		}
 		currentItem.closest(".xlistbox").trigger("change").trigger("select");
 		/*
@@ -306,7 +330,8 @@ function handlerClick(e) {
 			но вам нужны пользователи со старыми браузерами 
 		*/
 	}
-	$.data(currentItem.get(0), options)
+	parentOptions.items = itemsArray;
+	parentList.data(parentOptions);
 		/*
 			Могу переписать без делегирования
 		*/
@@ -401,6 +426,24 @@ console.log(l.xListBox("getItem", l.find("li").eq(0)));
 console.log(l.xListBox("setItem","order", {disabled: true}));
 */
 
+/*
+Все вместе
+console.log(l.xListBox("option", "disabled"));
+l.xListBox("option", {"disabled": true});
+console.log(l.xListBox("option", "disabled"));
+l.xListBox("option", "disabled", false);
+console.log(l.xListBox("option", "disabled"));
 
+var items = l.xListBox("items");//Написано, что возвращать в формате items
+console.log(items);
+items = items.reverse();
+console.log(items);
+l.xListBox("items", items);
 
+console.log(l.xListBox("getSelected"));
 
+console.log(l.xListBox("getItem","worker"));
+console.log(l.xListBox("getItem", l.find("li").eq(0)));
+
+console.log(l.xListBox("setItem","order", {disabled: true}));
+*/
